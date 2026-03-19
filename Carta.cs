a5ddba48b6dce63590be43ff71e5c507cc28f6e8
albervad <albervad@ucm.es>
2026-03-19 21:22:01 +0100

    Creada clase carta

--- Carta.cs
+++ Carta.cs
@@ -1,7 +1,24 @@
 using Godot;
 using System;
 
-public partial class Carta : Node
+[GlobalClass]
+public partial class Carta : Resource
 {
+	[Export] public string Palo { get; set; }
+	[Export] public string Nombre { get; set; }
 
+	public int GetValor() 
+	{
+		return Nombre switch
+		{
+			/* 
+			El controlador tiene un contador de ases y cada vez que te pasas 
+			de 21 resta 10 transformandolo en un 1 :)
+			*/
+			
+			"As" => 11, 
+			"K" or "Q" or "J" => 10,
+			_ => int.TryParse(Nombre, out int resultado) ? resultado : 0
+		};
+	}
 }
