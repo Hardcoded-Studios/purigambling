77d7a816678d1f43d98ce9d51df04952c62ba27b
albervad <albervad@ucm.es>
2026-03-24 14:01:41 +0100

    Añadidos atributos y constructora a carta

--- Carta.cs
+++ Carta.cs
@@ -1,24 +1,50 @@
 using Godot;
 using System;
+using System.Collections;
 
 [GlobalClass]
 public partial class Carta : Resource
 {
-	[Export] public string Palo { get; set; }
-	[Export] public string Nombre { get; set; }
+	[Export] public string Id { get; set; }
+    [Export] public string Suit { get; set; }
+	[Export] public string Name { get; set; }
+	[Export] public Godot.Collections.Array<int> EffectsId { get; set; }
+	[Export] public Texture2D Img { get; set; }
+	[Export] public string Desc { get; set; } 
 
-	public int GetValor() 
+	public Carta() 
 	{
-		return Nombre switch
+		this.Id = string.Empty;
+		this.Suit = string.Empty;
+		this.Name = string.Empty;
+		this.EffectsId = new Godot.Collections.Array<int>();
+		this.Img = null;
+		this.Desc = string.Empty;
+    }
+
+	public Carta(string id, string name, string suit, Godot.Collections.Array<string> effects, Texture2D img)
+	{
+        // Comprobamos que ningun parametro sea nulo o vacio
+        // Si un parametro es nulo o vacio lanzamos una expecion personalalizada para depurar mas facilmente
+		if (string.IsNullOrEmpty(id)) throw new ArgumentException("NULL CARD ID");
+		if (string.IsNullOrEmpty(name)) throw new ArgumentException("NULL CARD NAME");
+		if (string.IsNullOrEmpty(suit)) throw new ArgumentException("NULL CARD SUIT");
+		if (effects == null) throw new ArgumentException("NO CARD EFFECTS");
+		if (img == null) throw new ArgumentException("NO CARD IMG");
+    }
+
+    public int GetValor() 
+	{
+		return Name switch
 		{
 			/* 
-			El controlador tiene un contador de ases y cada vez que te pasas 
+				El controlador tiene un contador de ases y cada vez que te pasas 
 			de 21 resta 10 transformandolo en un 1 :)
 			*/
 			
 			"As" => 11, 
 			"K" or "Q" or "J" => 10,
-			_ => int.TryParse(Nombre, out int resultado) ? resultado : 0
+			_ => int.TryParse(Name, out int resultado) ? resultado : 0
 		};
 	}
 }
