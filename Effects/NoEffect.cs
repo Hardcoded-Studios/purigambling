a654bc7f326b25b2a344d72862a60092c7db2a97
albervad <albervad@ucm.es>
2026-03-25 18:46:04 +0100

    Fix: Se ha heredado Effect de resource, cambiado NoEffect a partial y arreglado la concatenacion de descripciones de cartas

--- Effects/NoEffect.cs
+++ Effects/NoEffect.cs
@@ -1,7 +1,6 @@
 using Godot;
-using System;
 
-public sealed class NoEffect : Effect
+public sealed partial class NoEffect : Effect
 {
 	public NoEffect() : base(GameConstants.Effect.NoEffectId, GameConstants.Effect.NoEffectName, GameConstants.Effect.NoEffectDescription) { }
 	public override void Apply(Card source) { }
